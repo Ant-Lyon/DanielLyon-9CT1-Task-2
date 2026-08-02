c32dbbe679c63924f1be3c86acc137ae07353f8e
Daniel Lyon <daniel.lyon@education.nsw.gov.au>
2026-08-02 14:16:42 +1000

    Update OpenMethod.py

--- OpenMethod.py
+++ OpenMethod.py
@@ -5,20 +5,20 @@
 # 'as' is used to set a value to a variable whilst a special action is being preformed;
 # This is done because you cannot define a variable to something that is being affected by a 'with' clause;
 # Don't even try.
-with open('workfile.txt', "r", encoding="utf-8") as file: # "utf-8" is the standard for .txt files*.
+with open('workfile.txt', "r", encoding="utf-8") as file: # "utf-8" is the standard for .txt files.
     message = file.read()
     print(message)
 # Automatically closes the file. Don't ask me why exactly it does that SPECIFICALLY,
-# I was too lazy to read more of the python doccumentation.
+# I was too lazy to read more of the python documentation.
 
 # I don't really know what an encoding IS, but I guess maybe it's some sort of codec?
 # Now that I think of it, they both have "cod" in them so maybe..
 
 # Extra Methods
-#file.close() # closes the file manually
+file.close() # closes the file manually
 file.closed # returns a boolean expression
 
-import locale # Don't ask me what this library is, it just is there in the second link
+import locale # Don't ask me what this library is, it just is there in the second link.
 locale.getencoding() # Find your device's locale encoding (usually utf-8)
 
 # Useless information below:
@@ -28,7 +28,7 @@ locale.getencoding() # Find your device's locale encoding (usually utf-8)
 # 3. Python converts it back based on your operating system
 
 # However if it is set to "", then it will display ALL line endings, but not change them,
-# and any  line ending you write will be unaffected.
+# and any line ending you write will be unaffected.
 
 # If it is say, "\r\n" (Windows default), it will only display that line ending,
 # and when you write any line ending it will convert it back to the one specified.
