7fe51e3cd83324d41002f8a28d0f410afc103f5b
Daniel Lyon <daniel.lyon@education.nsw.gov.au>
2026-08-14 15:08:07 +1000

    Update ModuleTesting.py

--- Pico/ModuleTesting.py
+++ Pico/ModuleTesting.py
@@ -8,25 +8,26 @@ print("4. Light dependent resistor")
 moduleInput = int(input("Which module would you like to test? "))
 
 if moduleInput == 1:
-    laser = Pin(21, Pin.OUT)
+    laser = Pin(16, Pin.OUT)
     while True:
         laser.value(1)
-        time.sleep(1)
+        time.sleep(3)
         laser.value(0)
         time.sleep(1)        
     
 elif moduleInput == 2:
-    redLed = Pin(21, Pin.OUT)
-    greenLed = Pin(22, Pin.OUT)
+    redLed = Pin(16, Pin.OUT)
+    greenLed = Pin(17, Pin.OUT)
     while True:
-        redLed.value(1)
-        time.sleep(1)
         redLed.value(0)
-        greenLed.value(1)
         time.sleep(1)
+        redLed.value(1)
+        greenLed.value(0)
+        time.sleep(1)
+        greenLed.value(1)
         
 elif moduleInput == 3:
-    buzzer = PWM(Pin(21))
+    buzzer = PWM(Pin(16))
     while True:
         buzzer.freq(440) # A4
         time.sleep(0.5)
